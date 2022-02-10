af5112e3d776775ab8294fef62ab2b46a686d4a7
Cadell Christo <git@cadell.dev>
2022-02-10 11:39:15 +0800

    take github and twitter hrefs as parameters

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,16 +2,22 @@ import React from "react";
 import { ThemeToggle } from "./themes";
 import { navAnchorClass, navClass, themeToggleHeaderClass } from "./styles.css";
 
-export const Header = () => {
+export const Header = ({
+	githubHref = "https://github.com/cadbox1",
+	twitterHref = "https://twitter.com/cadellchristo",
+}: {
+	githubHref?: string;
+	twitterHref?: string;
+}) => {
 	return (
 		<nav className={navClass}>
 			<a href="https://cadell.dev" className={navAnchorClass}>
 				Cadell.dev
 			</a>
-			<a href="https://github.com/cadbox1" className={navAnchorClass}>
+			<a href={githubHref} className={navAnchorClass}>
 				GitHub
 			</a>
-			<a href="https://twitter.com/cadellchristo" className={navAnchorClass}>
+			<a href={twitterHref} className={navAnchorClass}>
 				Twitter
 			</a>
 			<ThemeToggle className={themeToggleHeaderClass} />
