27a7734850d7dfe271935eb92948119fb04bc6e7
cadbox1 <hello@cadell.dev>
2023-11-23 12:43:45 +0000

    exclude dependencies and devdependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from "tsup";
 import { vanillaExtractPlugin } from "@vanilla-extract/esbuild-plugin";
-import { dependencies } from "./package.json";
+import { dependencies, devDependencies } from "./package.json";
 
 export default defineConfig({
 	entry: ["src/index.ts"],
 	outDir: "dist",
 	splitting: false,
 	bundle: true,
-    minify: false,
+	minify: false,
 	sourcemap: true,
 	format: ["cjs", "esm"],
-    dts: true,
+	dts: true,
 	target: "node12",
 	platform: "browser",
 	esbuildPlugins: [vanillaExtractPlugin()],
+	external: Object.keys(dependencies).concat(Object.keys(devDependencies)),
 	clean: true,
 });
